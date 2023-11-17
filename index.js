ce02db7c7b39d8e776d3d88c1721a8a99c878db7
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-11-17 20:51:24 +0530

    Change to nodejs

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const dotenv = require("dotenv").config();
+
+const RESEND_API_KEY = process.env.RESEND_API_KEY;
+
+app.use(express.json());
+app.use(cors({ origin: "*", methods: "GET, POST, PUT, DELETE, OPTIONS", allowedHeaders: "Content-Type, Authorization" }));
+
+app.post("/api/mail", async (req, res) => {
+  const { name, email, message } = req.body;
+  console.log(name, email, message, RESEND_API_KEY);
+  try {
+    const response = await fetch("https://api.resend.com/emails", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${RESEND_API_KEY}`,
+      },
+      body: JSON.stringify({
+        from: "Portfolio Mailing System <pms@resend.dev>",
+        to: ["waghmare.22111255@viit.ac.in"],
+        subject: `${name}`,
+        html: `<p>${message}</p><p>${email}</p>`,
+      }),
+    });
+    const responseJson = await response.json();
+
+    if (response.ok && response.status === 200) {
+      return res.status(200).json({ responseJson, payload: { name: name, email: email, message: message } });
+    } else {
+      return res.status(response.status).json({ error: "Resend error" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// Export the Express API
+module.exports = app;
