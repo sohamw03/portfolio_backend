fac598c1382e7e8c6ed2a771a27927e9cc7a8e9a
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-12-07 22:33:14 +0530

    Update padding in main div of EmailTemplate.js

--- template/EmailTemplate.js
+++ template/EmailTemplate.js
@@ -5,7 +5,7 @@ function emailTemplate(name, email, message) {
   <title>Email Template</title>
 </head>
 <body>
-  <div class="main" style="background-color: #10151d; font-family: Arial, sans-serif; margin: 3rem 1rem; border-radius: 15px;">
+  <div class="main" style="background-color: #10151d; font-family: Arial, sans-serif; padding: 4rem 1rem; border-radius: 15px;">
     <h1 style="font-weight: 400; font-size: 2.125rem; line-height: 1.235; letter-spacing: 0.00735em; color: #bfc7d2; text-align: center; margin: 3rem 0;">Portfolio Mailing System</h1>
     <div class="container" style="background-color: #10151d; max-width: 30rem; min-height: 15rem; margin: 0 auto; border: 1px solid #2e3c51; border-radius: 15px; padding: 0.5rem;">
       <div class="info" style="margin: 0.5rem; border-bottom: 1px solid rgb(46, 60, 82, 0.5);">
