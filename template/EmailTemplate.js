4906324a6b66c99675e13c795284bb05fe882405
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-12-07 22:29:17 +0530

    Refactor email template styles

--- template/EmailTemplate.js
+++ template/EmailTemplate.js
@@ -5,14 +5,14 @@ function emailTemplate(name, email, message) {
   <title>Email Template</title>
 </head>
 <body>
-  <div style="background-color: #10151d; font-family: Arial, sans-serif;">
+  <div class="main" style="background-color: #10151d; font-family: Arial, sans-serif; margin: 3rem 1rem; border-radius: 15px;">
     <h1 style="font-weight: 400; font-size: 2.125rem; line-height: 1.235; letter-spacing: 0.00735em; color: #bfc7d2; text-align: center; margin: 3rem 0;">Portfolio Mailing System</h1>
-    <div style="background-color: #10151d; max-width: 30rem; min-height: 15rem; margin: 0 auto; border: 1px solid #2e3c51; border-radius: 15px; padding: 0.5rem;">
-      <div style="margin: 0.5rem; border-bottom: 1px solid rgb(46, 60, 82, 0.5);">
-        <p style="font-weight: 500; font-size: 1.25rem; line-height: 1.6; letter-spacing: 0.0075em; color: #bfc7d2; margin: 0.5rem 0;">${name}</p>
-        <p style="font-size: 14px; font-family: Arial, sans-serif; max-width: 50rem; font-weight: 400; color: #808c9c; margin: 0.5rem 0;"><a href="mailto:${email}" style="color: #808c9c; text-decoration: none;">${email}</a></p>
+    <div class="container" style="background-color: #10151d; max-width: 30rem; min-height: 15rem; margin: 0 auto; border: 1px solid #2e3c51; border-radius: 15px; padding: 0.5rem;">
+      <div class="info" style="margin: 0.5rem; border-bottom: 1px solid rgb(46, 60, 82, 0.5);">
+        <p id="name" style="font-weight: 500; font-size: 1.25rem; line-height: 1.6; letter-spacing: 0.0075em; color: #bfc7d2; margin: 0.5rem 0;">${name}</p>
+        <p id="email" style="font-size: 14px; font-family: Arial, sans-serif; max-width: 50rem; font-weight: 400; color: #808c9c; margin: 0.5rem 0;"><a href="mailto:${email}" style="color: #808c9c; text-decoration: none;">${email}</a></p>
       </div>
-      <p style="color: #bfc7d2; margin: 0.5rem 0; margin-left: 0.5rem;">${message}</p>
+      <p id="message" style="color: #bfc7d2; margin: 0.5rem 0; margin-left: 0.5rem;">${message}</p>
     </div>
   </div>
 </body>
