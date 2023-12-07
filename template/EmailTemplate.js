c2b03d26ad576d329caf98c87b15e7a9dbb42474
Somatitoa <102748742+Somatitoa@users.noreply.github.com>
2023-12-07 22:40:32 +0530

    Fix margin-top in email template

--- template/EmailTemplate.js
+++ template/EmailTemplate.js
@@ -6,7 +6,7 @@ function emailTemplate(name, email, message) {
 </head>
 <body>
   <div class="main" style="background-color: #10151d; font-family: Arial, sans-serif; padding: 7rem 1rem; border-radius: 15px;">
-    <h1 style="font-weight: 400; font-size: 2.125rem; line-height: 1.235; letter-spacing: 0.00735em; color: #bfc7d2; text-align: center; margin-bottom: 3rem;">Portfolio Mailing System</h1>
+    <h1 style="font-weight: 400; font-size: 2.125rem; line-height: 1.235; letter-spacing: 0.00735em; color: #bfc7d2; text-align: center; margin-bottom: 3rem; margin-top: 0;">Portfolio Mailing System</h1>
     <div class="container" style="background-color: #10151d; max-width: 30rem; min-height: 15rem; margin: 0 auto; border: 1px solid #2e3c51; border-radius: 15px; padding: 0.5rem;">
       <div class="info" style="margin: 0.5rem; border-bottom: 1px solid rgb(46, 60, 82, 0.5);">
         <p id="name" style="font-weight: 500; font-size: 1.25rem; line-height: 1.6; letter-spacing: 0.0075em; color: #bfc7d2; margin: 0.5rem 0;">${name}</p>
